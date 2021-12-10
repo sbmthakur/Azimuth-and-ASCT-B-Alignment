b4a7bf6970d0c79b088294f2c4b044d039e2686b
Mahadevan Iyer <mahadevan3940@gmail.com>
2021-12-10 00:15:30 -0500

    dependedency Files added

---
+++ dependencies.R
@@ -0,0 +1,6 @@
+
+src<-'http://ftp.ussg.iu.edu/CRAN/'
+install.packages('jsonlite',repos = src)
+install.packages('tidyverse',repos = src)
+install.packages('dplyr',repos = src)
+install.packages('rjson',repos = src)
